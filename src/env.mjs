89a58c2a6b8c8acd709d224dd0d9d3e8f3961ba6
Raymart <raymartformalejo00@gmail.com>
2023-07-27 16:30:52 +0800

    auth

--- src/env.mjs
+++ src/env.mjs
@@ -21,7 +21,6 @@ export const env = createEnv({
       process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-   
   },
 
   /**
